cc76bd6a1e3e3579b54d9593ccc45b07d69e5642
Melvyn Laïly <melvyn.laily@gmail.com>
2020-05-27 10:10:19 +0200

    Move data definition to other file

---
+++ Data.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace _3DFun
+{
+    public static class Data
+    {
+        public static Model<Vector3> CreateCube() => new Model<Vector3>
+        {
+            Vertices = new List<Vector3>
+            {
+                new Vector3(-1, +1, +1), // 0
+                new Vector3(+1, +1, +1), // 1
+                new Vector3(+1, -1, +1), // 2
+                new Vector3(-1, -1, +1), // 3
+                    //                      // 
+                new Vector3(-1, +1, -1), // 4
+                new Vector3(+1, +1, -1), // 5
+                new Vector3(+1, -1, -1), // 6
+                new Vector3(-1, -1, -1), // 7
+            },
+            TemplatedFaces = new List<IReadOnlyList<int>>
+            {
+                new[] { 0, 1, 2, 3 },
+                new[] { 4, 5, 6, 7 },
+                new[] { 0, 4, 7, 3 },
+                new[] { 1, 5, 6, 2 },
+                // Top and bottom faces are missing since we are only doing wireframes for now...
+            }
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -30,35 +30,11 @@ namespace _3DFun
 
             static void Everyting(MainForm form)
             {
-
                 form.Text = "Cube";
 
                 // Model:
 
-                var cube = new Model<Vector3>
-                {
-                    Vertices = new List<Vector3>
-                    {
-                        new Vector3(-1, +1, +1), // 0
-                        new Vector3(+1, +1, +1), // 1
-                        new Vector3(+1, -1, +1), // 2
-                        new Vector3(-1, -1, +1), // 3
-                         //                      // 
-                        new Vector3(-1, +1, -1), // 4
-                        new Vector3(+1, +1, -1), // 5
-                        new Vector3(+1, -1, -1), // 6
-                        new Vector3(-1, -1, -1), // 7
-                    },
-                    TemplatedFaces = new List<IReadOnlyList<int>>
-                    {
-                        new[] { 0, 1, 2, 3 },
-                        new[] { 4, 5, 6, 7 },
-                        new[] { 0, 4, 7, 3 },
-                        new[] { 1, 5, 6, 2 },
-                        // Top and bottom faces are missing since we are only doing wireframes for now...
-                    }
-                };
-
+                var cube = Data.CreateCube();
                 var rotationCenter = Model.GetCenter(cube);
 
                 // Animation update timer:
